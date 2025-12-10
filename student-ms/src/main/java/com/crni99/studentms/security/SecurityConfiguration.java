028fabf552f2a8c1df0fa19764e187726b8923d3
izynaa <nur.mohdtalib@study.hs-duesseldorf.de>
2025-12-10 16:03:00 +0100

    Adjust security configuration for local development

--- student-ms/src/main/java/com/crni99/studentms/security/SecurityConfiguration.java
+++ student-ms/src/main/java/com/crni99/studentms/security/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.crni99.studentms.security;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,11 +10,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfiguration {
 
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
-		http.httpBasic(withDefaults());
-		return http.build();
-	}
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()   // <--- allow EVERYTHING
+            )
+            .httpBasic(withDefaults());
 
+        return http.build();
+    }
 }
